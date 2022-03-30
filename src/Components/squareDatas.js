48f428fee8be4154c3af80d180b461c4d78d1c7a
Ziggy <ziggyhand.dev@gmail.com>
2022-03-30 13:30:42 +0200

    initial commit

---
+++ src/Components/squareDatas.js
@@ -0,0 +1,20 @@
+export const squarePositions = [1.0, 1.0, -1.0, 1.0, 1.0, -1.0, -1.0, -1.0];
+
+export const squareColors = [
+  1.0,
+  1.0,
+  1.0,
+  1.0, // white
+  1.0,
+  0.0,
+  0.0,
+  1.0, // red
+  0.0,
+  1.0,
+  0.0,
+  1.0, // green
+  0.0,
+  0.0,
+  1.0,
+  1.0, // blue
+];
